9b4b0f1c303bed5a5663bb6593e8babfadcbc0b3
clément4444 <clement.fiquet0@gmail.com>
2025-01-02 17:49:29 +0100

    css du hedar home page

--- client/src/components/utilisateur/home/homeHeader/desciptionAnonce/DesciptionAnonce.tsx
+++ client/src/components/utilisateur/home/homeHeader/desciptionAnonce/DesciptionAnonce.tsx
@@ -1,5 +1,25 @@
+import style from "./desciptionAnonce.module.css";
+
 const DesciptionAnonce = () => {
-  return <div />;
+  return (
+    <div className={`${style.desciptionAnonce}`}>
+      {/* type de filme */}
+      <p className={`${style.headerType}`}>Movie</p>
+      {/* titre du filme */}
+      <p className={`${style.headerTitre}`}>Star Wars: The force Awaken</p>
+      {/* info filme */}
+      <p className={`${style.headerInfoFilme}`}>
+        2h40m • 2022 • Fantasy • Actions
+      </p>
+      {/* desciption */}
+      <p className={`${style.headerDescription}`}>
+        The third season of the American television series The Mandalorian stars
+        Pedro Pascal as the title character, a bounty hunter traveling to
+        Mandalore to redeem his past transgressions with his adopted son Grogu
+        and being aided on their journey by fellow Mandalorian Bo-Katan Kryze.
+      </p>
+    </div>
+  );
 };
 
 export default DesciptionAnonce;
