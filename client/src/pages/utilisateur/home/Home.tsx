60cb1d4901e3243c36c7d9190f059fc606e3cf44
philippevde <contact@van-der-eecken.eu>
2025-01-06 17:15:24 +0100

    footer

--- client/src/pages/utilisateur/home/Home.tsx
+++ client/src/pages/utilisateur/home/Home.tsx
@@ -1,11 +1,13 @@
 import HomeHeader from "../../../components/utilisateur/home/homeHeader/HomeHeader";
 import HomeHero from "../../../components/utilisateur/home/homeHero/HomeHero";
+import Footer from "../../../components/commun/footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <HomeHeader />
       <HomeHero />
+      <Footer />
     </div>
   );
 };
