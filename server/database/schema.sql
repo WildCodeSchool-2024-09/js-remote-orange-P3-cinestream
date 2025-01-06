5236b8b6a8d0e1a878b5d554e22d4131d28c8453
clément4444 <clement.fiquet0@gmail.com>
2025-01-06 12:23:26 +0100

    ajoute premier vertion de la dataBase + nettoyage des commentaire du mono repos

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,119 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+-- utilisateur
+CREATE TABLE utilisateur (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  mail VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  speudo VARCHAR(255) NOT NULL,
+  photo_profil VARCHAR(255) DEFAULT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+-- abonement
+CREATE TABLE abonement (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  actif TINYINT(1) DEFAULT 0,
+  date_fin DATE DEFAULT NULL,
+  utilisateur_id INT,
+  FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id)
 );
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
+-- univers
+CREATE TABLE univers (
+  id INT AUTO_INCREMENT PRIMARY KEY
+);
 
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+-- article
+CREATE TABLE article (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nom VARCHAR(255) NOT NULL,
+  DATE DATE DEFAULT NULL,
+  image VARCHAR(255) DEFAULT NULL,
+  image_rectangle VARCHAR(255) DEFAULT NULL,
+  publier TINYINT(1) DEFAULT 0,
+  premium TINYINT(1) DEFAULT 0,
+  univers_id INT DEFAULT NULL,
+  FOREIGN KEY (univers_id) REFERENCES univers(id)
+);
+
+-- favorie
+CREATE TABLE favorie (
+  article_id INT,
+  utilisateur_id INT,
+  PRIMARY KEY (article_id, utilisateur_id),
+  FOREIGN KEY (article_id) REFERENCES article(id),
+  FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id)
+);
+
+-- commentaire
+CREATE TABLE commentaire (
+  article_id INT,
+  utilisateur_id INT,
+  contenu TEXT NOT NULL,
+  DATE DATE NOT NULL,
+  PRIMARY KEY (article_id, utilisateur_id),
+  FOREIGN KEY (article_id) REFERENCES article(id),
+  FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id)
+);
+
+-- notes
+CREATE TABLE notes (
+  article_id INT,
+  utilisateur_id INT,
+  valeur INT NOT NULL,
+  PRIMARY KEY (article_id, utilisateur_id),
+  FOREIGN KEY (article_id) REFERENCES article(id),
+  FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id)
+);
+
+
+-- saison
+CREATE TABLE saison  (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numero INT NOT NULL,
+    article_id INT NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES article(id)
+);
+
+-- episode
+CREATE TABLE episode (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numero INT NOT NULL,
+    nom VARCHAR(255),
+    description TEXT,
+    lien_video VARCHAR(255),
+    image VARCHAR(255),
+    duree VARCHAR(10),
+    saison_id INT NOT NULL,
+    FOREIGN KEY (saison_id) REFERENCES saison(id)
+);
+
+-- plateforme
+CREATE TABLE plateforme (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    image VARCHAR(255)
+);
+
+-- categorie
+CREATE TABLE categorie (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    image VARCHAR(255)
+);
+
+-- plateforme_article
+CREATE TABLE plateforme_article (
+    article_id INT NOT NULL,
+    plateforme_id INT NOT NULL,
+    PRIMARY KEY (article_id, plateforme_id),
+    FOREIGN KEY (article_id) REFERENCES article(id),
+    FOREIGN KEY (plateforme_id) REFERENCES plateforme(id)
+);
+
+-- categorie_article
+CREATE TABLE categorie_article (
+    article_id INT NOT NULL,
+    categorie_id INT NOT NULL,
+    PRIMARY KEY (article_id, categorie_id),
+    FOREIGN KEY (article_id) REFERENCES article(id),
+    FOREIGN KEY (categorie_id) REFERENCES categorie(id)
+);
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,5 @@
-// Load environment variables from .env file
 import "dotenv/config";
-
-// Check database connection
-// Note: This is optional and can be removed if the database connection
-// is not required when starting the application
 import "../database/checkConnection";
-
-// Import the Express application from ./app
 import app from "./app";
 
 // Get the port from the environment variables
