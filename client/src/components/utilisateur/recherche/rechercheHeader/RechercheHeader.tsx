a475216ba80f65b0a6dd80326f72526c24f93fd6
philippevde <contact@van-der-eecken.eu>
2025-01-09 17:36:02 +0100

    Utilisateur Recherche

---
+++ client/src/components/utilisateur/recherche/rechercheHeader/RechercheHeader.tsx
@@ -0,0 +1,12 @@
+import styles from './rechercheHeader.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.recherchetitle}>Tous les films</h1>
+    </header>
+  );
+};
+
+export default Header;
+  
--- client/src/components/utilisateur/recherche/rechercheHero/filmComposent/filmComposent.tsx
+++ client/src/components/utilisateur/recherche/rechercheHero/filmComposent/filmComposent.tsx
@@ -1,5 +1,26 @@
-const filmComposent = () => {
-  return <div />;
+import styles from './filmComposent.module.css';
+
+const FilmComposent = () => {
+    // Exemple de données de films
+    const films = [
+        { id: 1, title: 'Film 1', image: '/temporaire/afficheSonic.jpg' },
+        { id: 2, title: 'Film 2', image: '/temporaire/afficheStarWars.jpg' },
+        { id: 3, title: 'Film 3', image: '/temporaire/afficheSuideGame.jpg' },
+        { id: 4, title: 'Film 4', image: '/temporaire/afficheTerminator.jpg' },
+        { id: 5, title: 'Film 5', image: '/temporaire/afficheSpectre.jpg' },
+        // Ajoutez plus de films ici
+    ];
+
+    return (
+        <div className={styles.filmComposent}>
+            {films.map(film => (
+                <div key={film.id} className={styles.filmCard}>
+                    <img src={film.image} alt={film.title} />
+                    <div className={styles.title}>{film.title}</div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
-export default filmComposent;
+export default FilmComposent;
