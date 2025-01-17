630b4126f4a02ec42af0f72f24f9aa9338b269b0
philippevde <contact@van-der-eecken.eu>
2025-01-17 11:43:27 +0100

    header page presentation

--- client/src/components/utilisateur/presentation/presentationHeader/bntRegarder/BntRegarder.tsx
+++ client/src/components/utilisateur/presentation/presentationHeader/bntRegarder/BntRegarder.tsx
@@ -1,5 +1,17 @@
+import style from './bntRegarder.module.css';
+
 const BntRegarder = () => {
-  return <div />;
+  return (
+    <button className={style.bntRegarder} type="button">
+      <div className={style.icon}>
+        <svg viewBox="0 0 24 24">
+          <title>Play Icon</title>
+          <path d="M8 5v14l11-7z" />
+        </svg>
+      </div>
+      Regarder
+    </button>
+  );
 };
 
 export default BntRegarder;
---
+++ client/src/components/utilisateur/presentation/presentationHero/PresentationHero.tsx
@@ -0,0 +1,13 @@
+import Episode from "./episode/Episode";
+import Univers from "./univers/Univers";
+
+const PresentationHero = () => {
+  return (
+    <>
+      <Episode />
+      <Univers />
+    </>
+  );
+};
+
+export default PresentationHero;
