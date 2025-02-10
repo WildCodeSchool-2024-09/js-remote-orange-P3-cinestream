274d425a62b0dbded9fd0c728018a6d3a5119340
clément4444 <clement.fiquet0@gmail.com>
2025-02-10 11:53:55 +0100

    création d'un menu burger pour le reponsive

--- client/src/components/commun/navBarre/NavBarre.tsx
+++ client/src/components/commun/navBarre/NavBarre.tsx
@@ -1,4 +1,7 @@
 import type React from "react";
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { TiThMenu } from "react-icons/ti";
 import { Link } from "react-router-dom";
 import { UseTokenContext } from "../../../context/tokenContext";
 import BntAdmin from "./bntAdmin/BntAdmin";
@@ -12,9 +15,11 @@ import Routes from "./routes/Routes";
 const NavBarre: React.FC = () => {
   //utiliser le contexte
   const { token, isAdmin } = UseTokenContext();
+  const [menu, setMenu] = useState(false);
 
   return (
     <div className={style.contenerNavBarre}>
+      {/* naveBarre pc */}
       <div className={style.navBarre}>
         {/* Logo à gauche */}
         <div className={style.logo}>
@@ -34,6 +39,41 @@ const NavBarre: React.FC = () => {
           {isAdmin && <BntAdmin />}
         </div>
       </div>
+
+      {/* naveBarre telphonne */}
+      <div className={style.navBarreMobile}>
+        {/* Logo à gauche */}
+        <div className={style.logo}>
+          <Link to={"/"}>
+            <img src="/images/logo/Cinestream.png" alt="Cinestream Logo" />
+          </Link>
+        </div>
+
+        <button
+          className={style.bntMenuBurger}
+          onClick={() => setMenu(!menu)}
+          type="button"
+        >
+          {!menu ? (
+            <TiThMenu className={style.IconMenuBurger} />
+          ) : (
+            <IoMdClose className={style.IconMenuBurger} />
+          )}
+        </button>
+      </div>
+      {/* menu burger */}
+      {menu && (
+        <div className={style.burger}>
+          <div className={style.boutonMobile}>
+            <BntRecherche />
+            {token && <BntCompte />}
+            {isAdmin && <BntAdmin />}
+          </div>
+          {!token && <BntIncription />}
+          {!token && <BntConnection />}
+          <Routes />
+        </div>
+      )}
     </div>
   );
 };
