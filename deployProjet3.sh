d42c4a948dfc7a62a3d1ae8bcce008c8bc8928ea
clément4444 <clement.fiquet0@gmail.com>
2025-02-15 11:23:26 +0100

    new cpanel

--- deployProjet3.sh
+++ deployProjet3.sh
@@ -5,15 +5,15 @@ export PATH="$PATH:/opt/alt/alt-nodejs20/root/usr/bin/"
 
 # Installer les dépendances
 cd ./client && npm install --production
-cd ./server && npm install --production
+cd ../server && npm install --production
 echo "Dependance installé"
 
 # Les build
-cd ./client && npm run build
-cd ./server && npm run build
+cd ../client && npm run build
+cd ../server && npm run build
 echo "Build terminé"
 
 # Déploye
-cp -R ./client/dist /home/ficl4036/public_html/projet3/
-cp -R ./server/dist /home/ficl4036/public_html/projet3-api/
+cp -R ~/02Projet3/client/dist ~/home/ficl4036/public_html/projet3/
+cp -R ~/02Projet3/server/dist ~/home/ficl4036/public_html/projet3-api/
 echo "Déploiement terminé"
