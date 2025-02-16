1167e19b82a8f4371946a75e22d1930e4be9fab5
clément4444 <clement.fiquet0@gmail.com>
2025-02-16 12:35:30 +0100

    adaptation url yt de telephone

--- client/src/hook/definirUrlVideoIframe.ts
+++ client/src/hook/definirUrlVideoIframe.ts
@@ -1,8 +1,17 @@
 function definirUrlVideoIframe(url: string): string {
-  if (url.includes("youtube")) {
-    const urlYoutube = url.split("watch?v=");
-    const urlYoutube2 = urlYoutube[1].split("&");
-    return `https://www.youtube.com/embed/${urlYoutube2[0]}`;
+  try {
+    if (url.includes("youtube")) {
+      const urlYoutube = url.split("watch?v=");
+      const urlYoutube2 = urlYoutube[1].split("&");
+      return `https://www.youtube.com/embed/${urlYoutube2[0]}`;
+    }
+    if (url.includes("youtu.be")) {
+      const urlYoutube = url.split("youtu.be/");
+      return `https://www.youtube.com/embed/${urlYoutube[1]}`;
+    }
+    //en case de crash
+  } catch (error) {
+    return url;
   }
   return url;
 }
